889350f872dc080d5630573d46ffc5a557275b6d
Roman Ivasyk <Kysavi7@gmail.com>
2020-05-29 01:49:11 +0300

    Base parameter check and usage display added.

--- csvdiff/Program.cs
+++ csvdiff/Program.cs
@@ -6,7 +6,26 @@ namespace csvdiff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args is null || args.Length <= 1)
+            {
+                ShowUsage();
+            }
+            if (args.Length >= 3 && args[2] != "-s")
+            {
+                Console.WriteLine($"Unknown argument - \"{args[2]}\"");
+                return;
+            }
+
+            //parallel loading of tables
+            //comparing
+            //output the result
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("\nUsage: csvdiff {<Table1>} {<Table2>} [-s [<directory_to_save>]]");
+            Console.WriteLine("-s  - Save the results to file [optional]");
+            Console.WriteLine("If the directory is not specified - the file will be saved to the directory with csvdiff utility.");
         }
     }
 }
